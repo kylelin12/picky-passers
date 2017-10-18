b063a64f26e34d88d15b2d7cfdf49ac1e56c58e6
Kyle Lin <klin12@stuy.edu>
2017-10-18 07:52:52 -0400

    Added comments

--- stu_mean.py
+++ stu_mean.py
@@ -6,6 +6,8 @@ f = ('secretdb.db')
 db = sqlite3.connect(f)
 c = db.cursor()
 
+# Returns the name and the grade associated with the name
+# in the format: "Name: Grade"
 def entries(name):
     s = ""
     entries = lookup(name)
@@ -13,6 +15,7 @@ def entries(name):
         s += str(entry[1]) + ": " + str(entry[2]) + " | "
     return s
 
+# Prints all data associated with the tuple passed to it
 def print_data(entries):
     for entry in entries:
         s = ""
@@ -20,11 +23,13 @@ def print_data(entries):
             s += str(data) + " | "
         print s
 
+# Returns the name, grades, and classes of the name passed to it.
 def lookup(name):
     c.execute("SELECT name, code, mark FROM peeps, courses WHERE courses.id = peeps.id AND '%s' = name;"%(name))
     result = c.fetchall()
     return result
 
+# Computes and returns the average of the name passed to it.
 def average(name):
     total = 0
     entries = lookup(name)
@@ -32,19 +37,25 @@ def average(name):
         total += int(entry[2])
     return total/len(entries)
 
+# Creates a table in the database to store averages
 def create_average_table():
     c.execute("CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, name TEXT, average INTEGER NOT NULL);")
     c.execute("SELECT name, id FROM peeps;")
     entries = c.fetchall()
+    # Inserts the averages from everybody in peeps into the averages table
+    #  in the format: <ID of Person>, <Name of Person>, <Average of Person>
     for entry in entries:
         c.execute("INSERT INTO peeps_avg VALUES(%s, '%s', %s)"%(entry[1], entry[0], average(entry[0])))
     print "Averages table created"
 
+# Returns a tuple containing all the averages found in the averages table
 def get_averages():
     c.execute("SELECT * FROM peeps_avg;")
     averages = c.fetchall()
     return averages
 
+# After changes are made to the courses (contains grades) database,
+# Updates the averages table with re-calculated averages.
 def update_averages():
     entries = get_averages()
     for entry in entries:
